0d50da3a05dcbc718707948c28200f7766fe78fa
Nguyen Ngan Anh <60727854+ngaananh@users.noreply.github.com>
2020-03-18 10:30:48 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,35 @@
-let m = Math.floor(Math.random() * 101);
+// let m = Math.floor(Math.random() * 101);
 
-for (let i = 1; i < 101; i++) {
-    let n = Number(prompt("mời bạn đoán"));
-    if (n > m) {
-        alert("Nhỏ hơn chút");
-	} else if (m > n) {
-		alert("Lớn hơn chút");
-	} else {
-		alert("Đúng");
-		break;
-    }
-    console.log(i);
+// for (let i = 1; i < 101; i++) {
+//     let n = Number(prompt("mời bạn đoán"));
+//     if (n > m) {
+//         alert("Nhỏ hơn chút");
+// 	} else if (m > n) {
+// 		alert("Lớn hơn chút");
+// 	} else {
+// 		alert("Đúng");
+// 		break;
+//     }
+//     console.log(i);
 
-}
-console.log(m);
+// }
+// console.log(m);
+
+
+
+// sv = [{"Tên":"Nguyễn Văn Anh", "Lớp":"12A", "Môn ưa thích":"Toán"},
+//             {"Tên":"Trần Thị Bình", "Lớp":"12B", "Môn ưa thích": "Lý"},
+//             {"Tên":"Bùi Văn C", "Lớp":"12C", "Môn ưa thích": "Hóa"},
+//             {"Tên":"Phạm Thị D", "Lớp":"12D", "Môn ưa thích": "Ngữ Văn"},
+//             {"Tên":"Hoàng Văn D", "Lớp":"12E", "Môn ưa thích": "Anh"}]
+// while (1==1){
+//     let i=prompt("bạn muốn biết hết thông tin về sinh viên nào:\n1. Nguyễn Văn A\n2. Trần Thị B\n3. Bùi Văn C\n4. Phạm Thị D\n5. Hoàng Văn D")
+//     if (i==0) {
+//         break
+//     }
+//     alert("Bạn "+sv[i-1]["Tên"]+" ở lớp "+sv[i-1]["Lớp"]+" thích môn "+sv[i-1]["Môn ưa thích"]+".")
+
+// }
 
 
 
